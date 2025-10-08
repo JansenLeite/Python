ba4692e2461002784c57f5b886aba5fc6bc4cfdb
JansenLeite <jansenleite@gmail.com>
2025-10-08 12:53:37 -0300

    Add files via upload

---
+++ PYTH_03_02_exemplo_1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+somar = num1 + num2
+
+print("O valor da SOMA é ", somar)
---
+++ PYTH_03_02_exemplo_3.py
@@ -0,0 +1,7 @@
+import exemplo_2
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+processa = exemplo_2.somar_dados(num1, num2)
+mostrar = exemplo_2.mostrar_dados(processa)
