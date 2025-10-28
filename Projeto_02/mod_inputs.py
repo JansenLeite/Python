c3c599cd8d800770c2cebc63c63b19480e2317a9
JansenLeite <jansenleite@gmail.com>
2025-10-28 20:01:59 -0300

    Add files via upload

--- Projeto_02/mod_inputs.py
+++ Projeto_02/mod_inputs.py
@@ -44,3 +44,34 @@ def inp_str_oper(mensagem):
         except:
             print("Campo inválido!")
     return inp
+
+def inp_int_destino(mensagem):
+    valida = True
+    while valida:
+        print("<<<<<< Informe o Aeroporto de Destino >>>>>>")
+        print("1 - Congonhas-SP")
+        print("2 - Guarulhos-SP")
+        print("3 - Campo de Marte-SP")
+        print("4 - Pinto Martins-CE")
+        print("5 - Eduardo Gomes-AM")
+        try:
+            inp = int(input(mensagem))
+            if inp == 1 or inp == 2 or inp == 3 or inp == 4 or inp == 5:
+                valida = False
+        except:
+            print("Campo inválido!")
+    return inp
+
+def inp_int_embalagem(mensagem):
+    valida = True
+    while valida:
+        print("<<<<<< Informe o Tipo de Embalagem >>>>>>")
+        print("1 - Caixa de Papelão")
+        print("2 - Container")
+        try:
+            inp = int(input(mensagem))
+            if inp == 1 or inp == 2:
+                valida = False
+        except:
+            print("Campo inválido!")
+    return inp
